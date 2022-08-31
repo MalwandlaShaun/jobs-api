423f8cc8c489750aaf9005287c31c989432c59b2
Malwandla Hlungwani <hlungwanimalwandlashaun@gmail.com>
2022-08-31 18:24:52 +0200

    initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,43 @@
+const User = require("../models/User")
+const { BadRequestError, UnauthenticatedError } = require('../errors')
+
+
+const {StatusCodes } = require("http-status-codes")
+
+const register = async(req,res) =>{
+
+    const user = await User.create({...req.body})
+
+    const token = user.createJWT()
+
+    res.status(StatusCodes.CREATED).json({name: user.name,token})    
+    
+    
+}
+
+const login = async(req,res) =>{
+
+    const {email,password} = req.body
+
+    if(!email || !password){
+        throw new BadRequestError("please provide email and password")
+    }
+
+    const user = await User.findOne({email})
+
+    if(!user){
+        throw new UnauthenticatedError("invalid credentials")
+    }
+    const token = user.createJWT()
+
+    const isPasswordCorrect = await user.comparePassword(password)
+    
+    if(!isPasswordCorrect){
+        throw new UnauthenticatedError("invalid credentials")
+
+    }
+
+    res.status(StatusCodes.OK).json({user: {name : user.name}, token})
+}
+
+module.exports = {register, login}
